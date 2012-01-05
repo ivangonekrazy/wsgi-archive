6f3f3f84fc38498c1e1206e12e4e935439336e97
Ivan Tam <ivangonekrazy@gmail.com>
2012-01-05 01:43:38 -0800

    added RAR support
    * also added more comments

--- xsection.py
+++ xsection.py
@@ -2,15 +2,30 @@ import sys, os.path
 import json
 import traceback
 from zipfile import ZipFile
+from rarfile import RarFile
 from wsgiref.validate import validator
 from wsgiref.simple_server import make_server
 from wsgiref.util import request_uri
 
+
+def ArchiveFile(path, mode='r'):
+    """ Abstract away the difference between zip and rars
+        ZipFile and RarFile have similar API's
+        makes this easy.
+    """
+
+    root, ext = os.path.splitext(path)
+
+    if ext.lower() == ".zip":
+        return ZipFile(path, mode)
+    elif ext.lower() == ".rar":
+        return RarFile(path, mode)
+
 def application(environ, start_response):
 
     def response(content, status="200 OK"):
-        """ Response builder; attempts to JSON-encode
-            non-strings
+        """ Response builder;
+            attempts to JSON-encode non-strings
         """
 
         if isinstance(content, str):
@@ -25,30 +40,39 @@ def application(environ, start_response):
             start_response(status, resp_headers)
             return [json_content]
 
-    path = environ.get('PATH_INFO').lstrip('/')
+    # remove the leading slash;
+    # don't confuse it with the root on the filesystem
+    archive_path = environ.get('PATH_INFO').lstrip('/')
 
-    if path == '':
+    # if no archive is given, stop here 
+    if archive_path == '':
         return response(str(e), '404 Not Found')
+
+    # skip favicons
+    if archive_path == 'favicon.ico':
+        return response("", '404 Not Found')
 
     try:
 
-        split_path = path.split('/', 1)
+        # split the archive path off,
+        # preserve the path within the archive
+        split_path = archive_path.split('/', 1)
 
         # just the JSON index of the archive
         if len(split_path) == 1:
-            zipfile = ZipFile(path, 'r')
+            archive = ArchiveFile(archive_path, 'r')
             return response({
-                "comment": zipfile.comment,
-                "files": sorted(zipfile.namelist() ),
-                "file_count": len(zipfile.namelist())
+                "comment": archive.comment,
+                "files": sorted(archive.namelist() ),
+                "file_count": len(archive.namelist())
             })
 
         # extract a file from the archive
         else:
-            zippath, file = split_path
-            zipfile = ZipFile(zippath, 'r')
+            archive_path, file = split_path
+            archive = ArchiveFile(archive_path, 'r')
             try:
-                return response( zipfile.read(file) )
+                return response( archive.read(file) )
             except KeyError, e:
                 return response(str(e), '404 Not Found')
 
